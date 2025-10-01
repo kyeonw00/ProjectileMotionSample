08b98be617e00e965a24c6c9e82b82599f579070
kyeonw00 <kang456026@gmail.com>
2025-10-01 19:20:09 +0900

    📝 added description comment

--- Assets/Scripts/Runtime/Unit.cs
+++ Assets/Scripts/Runtime/Unit.cs
@@ -6,7 +6,7 @@
  * 기반으로 개인 포트폴리오 용도로 재작성한 것입니다.
  * 
  * 본 코드에는 기밀 정보가 포함되어 있지 않으며,
-원본 프로젝트와는 별개로 동작합니다.
+ * 원본 프로젝트와는 별개로 동작합니다.
  *
  * Copyright (c) 강병준(github: kyeonw00), 
  * Licensed for personal portfolio and demonstration purposes only.
@@ -15,6 +15,10 @@
 
 using UnityEngine;
 
+/// <summary>
+/// 프로젝트 내에서 행동 가능한 오브젝트들을 나타내는 베이스 클래스
+/// !!!note!!!: 기능 예시를 보이기 위해 작성된 클래스입니다.
+/// </summary>
 public class Unit : MonoBehaviour
 {
     // Take amount of given damage.
